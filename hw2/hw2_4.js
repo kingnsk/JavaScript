86b95cab414e23e4d38067c1ecb3ed5f0ed3aeb8
kingnsk <kingnsk2020@gmail.com>
2021-07-05 23:35:07 +0700

    ДЗ к уроку №2

---
+++ hw2/hw2_4.js
@@ -0,0 +1,61 @@
+﻿// Урок2. Домашнее задание 4
+// Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от a до 15.
+var a=11;
+switch(a) {
+	case 0:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 1:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 2:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 3:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 4:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 5:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 6:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 7:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 8:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 9:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 10:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 11:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 12:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 13:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 14:
+		print_Numbers_from_A_to_15(a);
+		break;
+	case 15:
+		print_Numbers_from_A_to_15(a);
+		break;
+
+function print_Numbers_from_A_to_15(a) {
+	var msg = "";
+	for(var i=a; i<=15; i++) msg = msg + (i+" ");
+	alert(msg);	
+}  
+
+}
+
---
+++ hw2/hw2_8.js
@@ -0,0 +1,11 @@
+﻿//8. *С помощью рекурсии организовать функцию возведения числа в степень. Формат: function power(val, pow), где val – заданное число, pow – степень.
+
+
+alert(power(2,3));
+
+function power(val, pow) {
+	if(pow == 0) 	
+		return 1;
+	return val*power(val,pow-1);
+
+}
