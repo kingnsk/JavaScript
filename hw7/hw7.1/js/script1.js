4957bb824354a2b9eef7d8c287155a8df6f62d2b
kingnsk <kingnsk2020@gmail.com>
2021-07-31 00:14:42 +0700

    Выполнено задание 7.1

--- hw7/hw7.1/js/script1.js
+++ hw7/hw7.1/js/script1.js
@@ -1,9 +1,11 @@
 var products = [
-	{name : "Sprite", price : 90},
+    {name : "Sprite", price : 90},
     {name : "Fanta", price : 85},
     {name : "Juice", price : 110},
     {name : "Water", price : 25},
 ]
+
+var deletedPrefix = "deleted";
 
 var basket = {
     goods: [
@@ -18,34 +20,73 @@ var basket = {
     },
     addProduct: function(newProduct) {                     
         this.goods.push(newProduct);
+    },
+    removeProduct(index) {
+        if (index == 0) {
+            this.goods.shift();
+        } else {
+            this.goods.splice(index, index);
+        }
+    },
+    updateBasket: function() {
+        var basket =document.getElementById("basket");
+        basket.innerHTML = '';
+        for (var i = 0; i < this.goods.length; i++) {
+            var item = document.createElement("ul");
+            item.innerHTML = this.goods[i].name + " " + this.goods[i].price + " ";
+            var button = document.createElement("button");
+            button.innerHTML = "-";
+            button.id = deletedPrefix + i;
+            button.addEventListener("click", deleteFromBasket)
+            item.appendChild(button);
+            basket.appendChild(item);
+        }
         var sum = this.countBasketPrice();
-        var basket = document.getElementById("basket");
-        var item = document.createElement("ul");
-        item.innerHTML = newProduct.name + " " + newProduct.price;
-        basket.appendChild(item);
         var totalSum = document.getElementById("totalSum");
         totalSum.innerHTML = sum;
     }
+
+
+
 }
 
 function addToBasket(event) {
     const targetButton = event.target;
     let index = targetButton.id;
     basket.addProduct(products[index]);
+    basket.updateBasket();
 }
+
+function deleteFromBasket(event) {
+    const targetButton = event.target;
+    const li = targetButton.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
+    let indexStr = targetButton.id;
+    indexStr = indexStr.slice(deletedPrefix.length);
+    let index = parseInt(indexStr);
+    basket.removeProduct(index);
+    basket.updateBasket();
+}
+
+
 
 function init() {
 
     var list = document.getElementById("productList");
     for (var i = 0; i < products.length; i++) {
         var item = document.createElement("li");
-        var button = document.createElement("button");
-        button.innerHTML = "Добавить в корзину";
-        button.id = i;
-        button.addEventListener("click", addToBasket)
+        var buttonAdd = document.createElement("button");
+        buttonAdd.innerHTML = "+";
+	buttonAdd.style.width = "20px";
+	buttonAdd.style.height = "20px";
+        buttonAdd.id = i;
+        buttonAdd.addEventListener("click", addToBasket)
+
         item.innerHTML = products[i].name + "&nbsp(" + products[i].price + ")&nbsp&nbsp&nbsp";
         list.appendChild(item);
-        item.appendChild(button);
+        item.appendChild(buttonAdd);
+	
     }
 	var totalSum = document.getElementById("totalSum");
 	totalSum.innerText = 0;
